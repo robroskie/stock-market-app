b3b6c9d87dcdd7e61de23e31ca890f6a4b17b16f
robroskie <robroskie@gmail.com>
2022-07-01 15:21:20 -0700

    updates to html

--- finance.py
+++ finance.py
@@ -1,11 +1,26 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 msft = yf.Ticker("MSFT")
 
 # get stock info
 msft.info
 
+startDate = '2020-01-01'
+endDate = '2022-12-01'
+
 # get historical market data
-hist = msft.history(period="max")
+hist = msft.history(start=startDate, end=endDate)
+
+
 
 print(hist)
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import mplfinance as mpf
+
+plt = mpf.plot(hist, savefig = 'templates/images/temp/plot.png')
