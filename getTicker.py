bf259973c9d2aed7fe89882302c997c83c1d568f
robroskie <robroskie@gmail.com>
2022-07-09 22:38:06 -0700

    removed api key and added to config.py file

--- getTicker.py
+++ getTicker.py
@@ -1,10 +1,11 @@
 import requests
 import json
+from config import api_key
 
 def getTicker (company_name):
-    api_key = '195BBOTV47629JVY'
+    key = api_key
     # send a GET request with the company name as keyword in the url 
-    res = requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={company_name}&apikey={api_key}')
+    res = requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={company_name}&apikey={key}')
 
     response = res.json()
     # print(response)
